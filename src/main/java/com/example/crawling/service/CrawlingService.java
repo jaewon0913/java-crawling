831011c25de9c3842dbc55d421bd172a13024861
jaewon0913 <jaewon9937@gmail.com>
2023-04-14 15:07:37 +0900

    Feat: 크롤링
    - 소스 정리

--- src/main/java/com/example/crawling/service/CrawlingService.java
+++ src/main/java/com/example/crawling/service/CrawlingService.java
@@ -11,26 +11,16 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CrawlingService {
-
-    private final WebDriver webDriver;//셀리니움 사용을 위한 webDriver 주입
     private static String WEB_DRIVER_ID = "webdriver.chrome.driver";//property 키
     private static String WEB_DRIVER_PATH = "C:\\Users\\11242\\Desktop\\study\\crawling\\src\\main\\resources\\static\\selenium/chromedriver.exe";
 
     public CrawlingService() {
-        System.setProperty(WEB_DRIVER_ID,WEB_DRIVER_PATH);
 
-        ChromeOptions options=new ChromeOptions();
-        options.addArguments("--start-maximized");
-        options.addArguments("--disable-popup-blocking");//팝업 무시
-        options.addArguments("headless");// 창이없이 프로세스 사용
-        options.addArguments("--remote-allow-origins=*");
-        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-
-        this.webDriver = new ChromeDriver(options);
     }
 
     public void crawling(String bank){
@@ -43,25 +33,44 @@ public class CrawlingService {
 
     private void seleniumCrawl(){
 
+        //  메소드 실행 시마다 새로 세팅해서 WebDriver 가 호출이 되어야 할 것 같음
+        System.setProperty(WEB_DRIVER_ID,WEB_DRIVER_PATH);
+
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("--disable-popup-blocking");//팝업 무시
+        options.addArguments("headless");// 창이없이 프로세스 사용
+        options.addArguments("--remote-allow-origins=*");
+        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+
+        WebDriver webDriver = new ChromeDriver(options);
+
+        //  목록에서 상품 상세화면 링크 저장
+        List<String> depositLinkList = new ArrayList<>();
+        List<String> savingLinkList = new ArrayList<>();
+
         try {
+            //  목록화면 URL
             webDriver.get("https://www.kebhana.com/cont/mall/mall08/mall0805/index.jsp?_menuNo=62608");
 
             Thread.sleep(2000);
 
+            //  상품 조회
             List<WebElement> deposit
-                    = webDriver.findElements(By.cssSelector("#contents > div.banking-content > div.banking-row-area > div.wrap-product-list > ul"));
+                    = webDriver.findElements(By.cssSelector("#contents > div.banking-content > div.banking-row-area > div.wrap-product-list > ul > li"));
 
             for (WebElement element : deposit) {
                 System.out.println("----------------------------");
                 System.out.println(element.getText());
+                depositLinkList.add(element.findElement(By.tagName("a")).getAttribute("href").toString());
             }
 
+            //  적금 버튼 누르기 위한 WebElement
             WebElement webElement
                     = webDriver.findElement(By.xpath("//*[@id=\"search\"]/ul/li[1]/div/ul/li[2]/a"));
 
-            System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
             System.out.println(webElement.getText());
-
+            //  적금 버튼 클릭
             if(webElement.getText().equals("적 금")) {
                 JavascriptExecutor javascriptExecutor = (JavascriptExecutor) webDriver;
                 javascriptExecutor.executeScript("arguments[0].click();", webElement);
@@ -70,17 +79,26 @@ public class CrawlingService {
             Thread.sleep(2000);
 
             List<WebElement> saving
-                    = webDriver.findElements(By.cssSelector("#contents > div.banking-content > div.banking-row-area > div.wrap-product-list > ul"));
+                    = webDriver.findElements(By.cssSelector("#contents > div.banking-content > div.banking-row-area > div.wrap-product-list > ul > li"));
                     //= webDriver.findElements(By.className("item type2"));
 
             for (WebElement element : saving) {
                 System.out.println("@@@@@@@@@@@@@@@@@@");
                 System.out.println(element.getText());
+                savingLinkList.add(element.findElement(By.tagName("a")).getAttribute("href").toString());
             };
 
             webDriver.quit();
         } catch (Exception e){
             e.printStackTrace();
+        }
+
+        for(String link : depositLinkList) {
+            System.err.println(link);
+        }
+
+        for(String link : savingLinkList) {
+            System.err.println(link);
         }
     }
 
